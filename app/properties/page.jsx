f964330b32c7b730125a719cfcfcf55220a4e298
unknown <samiizadeh.ali@gmail.com>
2024-10-31 01:18:34 +0330

    properties.json file removed and database added

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,7 +1,24 @@
-import properties from "@/properties.json";
+// import properties from "@/properties.json";
 import PropertyCard from "@/components/propertyCard";
 
-const PropertiesPage = () => {
+const fetchProperties = async () => {
+  try {
+    const res = await fetch("http://localhost:3000/api/properties");
+    if (!res.ok) {
+      throw new Error("Faild to fetch");
+    }
+    return res.json();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const PropertiesPage = async () => {
+  const properties = await fetchProperties();
+
+  properties.sort((a, b) => {
+    new Date(b.createdAt) - new Date(a.createdAt);
+  });
   return (
     <section className="px-4 py-6">
       <div className="container-xl lg:container m-auto">
--- components/HomeProperties.jsx
+++ components/HomeProperties.jsx
@@ -1,8 +1,22 @@
-import properties from "@/properties.json";
+// import properties from "@/properties.json";
 import PropertyCard from "./PropertyCard";
 import Link from "next/link";
 
-const HomeProperties = () => {
+const fetchProperties = async () => {
+  try {
+    const res = await fetch("http://localhost:3000/api/properties");
+    if (!res.ok) {
+      throw new Error("Faild to fetch");
+    }
+    return res.json();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const HomeProperties = async () => {
+  const properties = await fetchProperties();
+
   const recentProperties = properties
     .sort(() => {
       Math.random() - Math.random();
