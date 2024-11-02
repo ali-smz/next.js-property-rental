2c10b0288d0114ceac6b051944640daec2a49d7a
unknown <samiizadeh.ali@gmail.com>
2024-11-02 21:49:25 +0330

    property page codes cleaned

--- components/PropertyDetail.jsx
+++ components/PropertyDetail.jsx
@@ -96,9 +96,6 @@ const PropertyDetail = ({ property }) => {
           ))}
         </ul>
       </div>
-      <div className="bg-white p-6 rounded-lg shadow-md mt-6">
-        <div id="map"></div>
-      </div>
     </main>
   );
 };
