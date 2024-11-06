4e48b9f897be1ac9c42c2a1c0b3d977ed591e0a9
unknown <samiizadeh.ali@gmail.com>
2024-11-06 23:31:28 +0330

    post request for property add form developed

--- app/api/properties/route.js
+++ app/api/properties/route.js
@@ -11,3 +11,50 @@ export const GET = async (request) => {
     return new Response(error, { status: 500 });
   }
 };
+
+export const POST = async (request) => {
+  try {
+    const formData = await request.formData();
+    const amenities = formData.getAll("amenities");
+    const images = formData
+      .getAll("images")
+      .filter((image) => image.name !== "");
+
+    const propertyData = {
+      type: formData.get("type"),
+      name: formData.get("name"),
+      description: formData.get("description"),
+      location: {
+        street: formData.get("location.street"),
+        city: formData.get("location.city"),
+        state: formData.get("location.state"),
+        zipcode: formData.get("location.zipcode"),
+      },
+      beds: formData.get("beds"),
+      baths: formData.get("baths"),
+      square_feet: formData.get("square_feet"),
+      amenities,
+      rates: {
+        weekly: formData.get("rates.weekly"),
+        monthly: formData.get("rates.monthly"),
+        nightly: formData.get("rates.nightly."),
+      },
+      seller_info: {
+        name: formData.get("seller_info.name"),
+        email: formData.get("seller_info.email"),
+        phone: formData.get("seller_info.phone"),
+      },
+      images,
+    };
+
+    console.log(propertyData);
+    return new Response(JSON.stringify({ message: "success" }), {
+      status: 200,
+    });
+  } catch (error) {
+    console.log(error);
+    return new Response(JSON.stringify({ message: "faild to add property" }), {
+      status: 500,
+    });
+  }
+};
