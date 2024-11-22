3e628a75ece41cbd9eceec275a867a0277aea1d7
unknown <samiizadeh.ali@gmail.com>
2024-11-23 00:46:03 +0330

    query added

--- components/PropertySearchForm.jsx
+++ components/PropertySearchForm.jsx
@@ -1,11 +1,27 @@
+"use client";
 import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 const PropertySearchForm = () => {
   const [location, setLocation] = useState("");
   const [propertyType, setPropertyType] = useState("All");
+  const router = useRouter();
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    if ((location === "", propertyType === "All")) {
+      router.push("/properties");
+    } else {
+      const query = `?location=${location}&propertyType=${propertyType}`;
+      router.push(`/properties/search-results${query}`);
+    }
+  };
 
   return (
-    <form className="mt-3 mx-auto max-w-2xl w-full flex flex-col md:flex-row items-center">
+    <form
+      className="mt-3 mx-auto max-w-2xl w-full flex flex-col md:flex-row items-center"
+      onSubmit={submitHandler}
+    >
       <div className="w-full md:w-3/5 md:pr-2 mb-4 md:mb-0">
         <label htmlFor="location" className="sr-only">
           Location
@@ -15,6 +31,8 @@ const PropertySearchForm = () => {
           id="location"
           placeholder="Enter Location (City, State, Zip, etc)"
           className="w-full px-4 py-3 rounded-lg bg-white text-gray-800 focus:outline-none focus:ring focus:ring-blue-500"
+          value={location}
+          onChange={(e) => setLocation(e.target.value)}
         />
       </div>
       <div className="w-full md:w-2/5 md:pl-2">
@@ -24,6 +42,10 @@ const PropertySearchForm = () => {
         <select
           id="property-type"
           className="w-full px-4 py-3 rounded-lg bg-white text-gray-800 focus:outline-none focus:ring focus:ring-blue-500"
+          onChange={(e) => {
+            setPropertyType(e.target.value);
+          }}
+          value={propertyType}
         >
           <option value="All">All</option>
           <option value="Apartment">Apartment</option>
