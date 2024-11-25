6a439aae02d7c4d8214e40a0b91aca01e8c59a0e
unknown <samiizadeh.ali@gmail.com>
2024-11-26 01:25:16 +0330

    message model created

---
+++ models/Message.js
@@ -0,0 +1,43 @@
+import { model, models, Schema } from "mongoose";
+
+const MessageSchema = new Schema(
+  {
+    sender: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      require: true,
+    },
+    recipient: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      require: true,
+    },
+    sender: {
+      type: Schema.Types.ObjectId,
+      ref: "Property",
+      require: true,
+    },
+    name: {
+      type: String,
+      require: [true, "Name is required"],
+    },
+    email: {
+      type: String,
+      require: [true, "Email is required"],
+    },
+    phone: {
+      type: String,
+    },
+    body: {
+      type: String,
+    },
+    read: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+const Message = models.Message || model("Message", MessageSchema);
+export default Message;
